685253c65b1add581b562cb3bf7fb7d404440613
Rabi <mahmod.rabi@philips.com>
2016-12-18 13:07:27 +0200

    Initial Commit

---
+++ spring-cloud-config-server-postgresql/src/main/java/org/springframework/cloud/config/server/postgresql/config/PostgresqlEnvironmentRepositoryConfiguration.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.config.server.postgresql.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+import org.springframework.cloud.config.server.environment.NativeEnvironmentRepository;
+import org.springframework.cloud.config.server.environment.SearchPathLocator;
+import org.springframework.cloud.config.server.postgresql.environment.PostgresqlEnvironmentRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * @author Venil Noronha
+ */
+@Configuration
+public class PostgresqlEnvironmentRepositoryConfiguration {
+
+	@Autowired
+	private ConfigurableEnvironment environment;
+	
+    @Autowired
+    private DataSource dataSource;
+
+	@Bean
+	public SearchPathLocator searchPathLocator() {
+		return new NativeEnvironmentRepository(environment);
+	}
+
+	@Bean
+	@Primary
+	public EnvironmentRepository environmentRepository() {
+		return new PostgresqlEnvironmentRepository(dataSource);
+	}
+
+}
